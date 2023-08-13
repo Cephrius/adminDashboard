9a1512bde2c94d724d9b15d924f91701bb6c54e1
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-08-13 09:44:48 -0500

    first commit

---
+++ orders.js
@@ -0,0 +1,22 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        prouductNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+
+    {
+        productName: 'CSS Full Tutorial',
+        prouductNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+
+    {
+        productName: 'React.JS Tutorial',
+        prouductNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
